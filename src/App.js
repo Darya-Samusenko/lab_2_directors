c24ca387db395a2a9f20e54d627f65371bd7c218
Samusenko_D <loneluna@mail.ru>
2022-12-19 15:03:17 +0300

    Replace files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import Header from "./components/Header";
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import transResources from './components/transl'
+
+import Home from "./pages/Home";
+import AWriter from "./pages/AWriter"
+import Writers from "./pages/Writers";
+
+i18n
+.use(initReactI18next)
+.use(LanguageDetector)
+.init({
+  supportedLngs: ['ru', 'en'],
+  fallbackLng: "ru",
+  resources: transResources
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+     <Router>
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="/Writers" element={<Writers/>}/>
+                    <Route exact path = '/Kupala' element = {<AWriter writer = "kupala"/>}/>
+                    <Route exact path = '/Kolas' element = {<AWriter writer = "kolas"/>}/>
+                    <Route exact path = '/Tank' element = {<AWriter writer = "tank"/>}/>
+                    <Route exact path = '/Korotkevich' element = {<AWriter writer = "korotkevich"/>}/>
+                    <Route exact path = '/Bagdanovich' element = {<AWriter writer = "bagdanovich"/>}/>
+                    <Route exact path = '/Melez' element = {<AWriter writer = "melez"/>}/>
+                </Routes>
+            </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const AppClass = () => {
+  const [dimensions, setDimensions] = React.useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
+  const handleResize = () => {
+    setDimensions({
+      height: window.innerHeight,
+      width: window.innerWidth
+    });
+  }
+  React.useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => { window.removeEventListener('resize', handleResize) }
+  });
+  return (
+    <App/>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Suspense fallback = {null}>
   <React.StrictMode>
-    <App />
+    <AppClass />
   </React.StrictMode>
+  </Suspense>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
